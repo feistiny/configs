73b8ad271b5e7266362bd7cd86109494e0f5618c
liuzhanfei <liuzhanfei166@126.com>
2018-05-19 11:22:08 +0800

    grep docker then stop/rm

--- aliases.sh
+++ aliases.sh
@@ -5,32 +5,40 @@ alias cll='clear; ls -al'
 alias cla='clear; ls -a'
 
 # git aliases
-alias gst='git status'
+alias gad='git add'
+alias gbr='git branch'
+alias gci='git commit'
 alias gco='git checkout'
 alias gdf='git diff'
 alias gfe='git fetch'
+alias gll='git log --oneline'
 alias gpu='git push'
 alias gre='git rebase'
-alias gad='git add'
-alias gbr='git branch'
-alias gll='git log --oneline'
 alias grh='git reset HEAD'
-alias gci='git commit'
+alias gst='git status'
 
 # docker aliases
 alias dcm="docker-compose"
 alias dex='docker exec -it'
 alias dim="docker images"
 alias din='docker inspect'
+alias dlg='docker logs'
 alias dps='docker ps'
 alias drm='docker rm $(docker ps -aq)'
 alias drmi='docker rmi'
-alias dstop='docker stop $(docker ps -aq)'
-alias dlg='docker logs'
+alias dstp='docker stop $(docker ps -aq)'
 
 
 function dlog() {
     docker logs laradock_${1}_1
+}
+
+function dfstp() {
+    docker stop $(docker ps | grep $1 | awk '{print $1}') 2>/dev/null
+}
+
+function dfrm() {
+    docker rm $(docker ps | grep $1 | awk '{print $1}') 2>/dev/null
 }
 
 function dexec() {
