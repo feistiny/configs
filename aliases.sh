33c9009fc236fe11c6f3359e719575005cca2ac9
lzf <liuzhanfei166@126.com>
2018-07-20 13:01:06 +0800

    not to confuse others who not konwing git is rewrote with gdr feature.

--- aliases.sh
+++ aliases.sh
@@ -284,7 +284,7 @@ function git_dir_worktree() {
     fi
   fi
 }
-if [ -n "$(gets current_git_dir)" ]; then gdr $(gets current_git_dir); fi
+# if [ -n "$(gets current_git_dir)" ]; then gdr $(gets current_git_dir); fi
 
 
 function gls() {
